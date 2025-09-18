14f0fbc9b7f38414a80c9e24b861f3984598935f
Robert Zaremba <robert@zaremba.ch>
2025-09-18 21:24:34 +0200

    CI: reorganize formatting jobs (#142)
    * CI: reorganize formatting jobs
    * update prettier setting
    * format
    
    ---------
    
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- packages/bitcoin_spv/README.md
+++ packages/bitcoin_spv/README.md
@@ -18,9 +18,9 @@ Bitcoin Light Client for [Native](https://gonative.cc) on [Sui](https://sui.io/)
 
 ### Native Alpha
 
--   Version: [#60a1ee2](https://github.com/gonative-cc/move-bitcoin-spv/tree/60a1ee2cb8685c9c7494d5e5f7640b9427375748) (branch: `release/alpha`)
--   Package ID: `0xc31478c4cc6cc146a7ecfc9cc5096d4421d675bdf5577cb7e550392a7cb93dc5`
--   SPV Object ID: `0x4f989d395bb13b4913b483016641eb7c9cacfd88d2a1ba91523d0542a52af9e4`
+- Version: [#60a1ee2](https://github.com/gonative-cc/move-bitcoin-spv/tree/60a1ee2cb8685c9c7494d5e5f7640b9427375748) (branch: `release/alpha`)
+- Package ID: `0xc31478c4cc6cc146a7ecfc9cc5096d4421d675bdf5577cb7e550392a7cb93dc5`
+- SPV Object ID: `0x4f989d395bb13b4913b483016641eb7c9cacfd88d2a1ba91523d0542a52af9e4`
 
 ## Setup
 
@@ -28,5 +28,5 @@ Check out [setup](../../scripts/SETUP.md) for step-by-step deployment.
 
 ## Talk to us
 
--   Follow the Native team's activities on the [Native X/Twitter account](https://x.com/NativeNetwork).
--   Join the conversation on [Native Discord](https://discord.gg/gonative).
+- Follow the Native team's activities on the [Native X/Twitter account](https://x.com/NativeNetwork).
+- Join the conversation on [Native Discord](https://discord.gg/gonative).
--- scripts/new_light_client.js
+++ scripts/new_light_client.js
@@ -6,64 +6,64 @@ import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import "dotenv/config";
 
 function loadSigner() {
-	if (process.env.ENCODE_SK) {
-		let sk = fromBase64(process.env.ENCODE_SK);
-		return Ed25519Keypair.fromSecretKey(sk.slice(1));
-	}
-	if (process.env.MNEMONIC) {
-		return Ed25519Keypair.deriveKeypair(process.env.MNEMONIC);
-	}
-	throw new Error(
-		"Missing required environment variable: Please set either ENCODE_SK or MNEMONIC."
-	);
+  if (process.env.ENCODE_SK) {
+    let sk = fromBase64(process.env.ENCODE_SK);
+    return Ed25519Keypair.fromSecretKey(sk.slice(1));
+  }
+  if (process.env.MNEMONIC) {
+    return Ed25519Keypair.deriveKeypair(process.env.MNEMONIC);
+  }
+  throw new Error(
+    "Missing required environment variable: Please set either ENCODE_SK or MNEMONIC.",
+  );
 }
 
 async function main() {
-	const PACKAGE_ID = process.env.PACKAGE_ID;
-	const client = new SuiClient({ url: getFullnodeUrl(process.env.NETWORK) });
+  const PACKAGE_ID = process.env.PACKAGE_ID;
+  const client = new SuiClient({ url: getFullnodeUrl(process.env.NETWORK) });
 
-	const signer = loadSigner();
-	const tx = new Transaction();
-	const raw_headers = process.env.HEADERS.split(",");
-	let headers = [];
-	for (let i = 0; i < raw_headers.length; ++i) {
-		const header = tx.moveCall({
-			target: `${PACKAGE_ID}::block_header::new_block_header`,
-			arguments: [tx.pure("vector<u8>", fromHex(raw_headers[i]))],
-		});
-		headers.push(header);
-	}
+  const signer = loadSigner();
+  const tx = new Transaction();
+  const raw_headers = process.env.HEADERS.split(",");
+  let headers = [];
+  for (let i = 0; i < raw_headers.length; ++i) {
+    const header = tx.moveCall({
+      target: `${PACKAGE_ID}::block_header::new_block_header`,
+      arguments: [tx.pure("vector<u8>", fromHex(raw_headers[i]))],
+    });
+    headers.push(header);
+  }
 
-	const header_vec = tx.makeMoveVec({
-		type: `${PACKAGE_ID}::block_header::BlockHeader`,
-		elements: headers,
-	});
+  const header_vec = tx.makeMoveVec({
+    type: `${PACKAGE_ID}::block_header::BlockHeader`,
+    elements: headers,
+  });
 
-	tx.moveCall({
-		target: `${PACKAGE_ID}::light_client::initialize_light_client`,
-		arguments: [
-			tx.pure.u8(process.env.BTC_NETWORK),
-			tx.pure.u64(process.env.BTC_HEIGHT),
-			header_vec,
-			tx.pure.u256(process.env.PARENT_CHAIN_WORK),
-			tx.pure.u64(process.env.FINALITY),
-		],
-	});
+  tx.moveCall({
+    target: `${PACKAGE_ID}::light_client::initialize_light_client`,
+    arguments: [
+      tx.pure.u8(process.env.BTC_NETWORK),
+      tx.pure.u64(process.env.BTC_HEIGHT),
+      header_vec,
+      tx.pure.u256(process.env.PARENT_CHAIN_WORK),
+      tx.pure.u64(process.env.FINALITY),
+    ],
+  });
 
-	let res = await client.signAndExecuteTransaction({
-		transaction: tx,
-		signer,
-		options: {
-			showEffects: true,
-			showEvents: true,
-		},
-	});
+  let res = await client.signAndExecuteTransaction({
+    transaction: tx,
+    signer,
+    options: {
+      showEffects: true,
+      showEvents: true,
+    },
+  });
 
-	await client.waitForTransaction({ digest: res.digest });
+  await client.waitForTransaction({ digest: res.digest });
 
-	console.log(JSON.stringify(res.events[0].parsedJson, null, 2));
+  console.log(JSON.stringify(res.events[0].parsedJson, null, 2));
 }
 
 main()
-	.then(() => {})
-	.catch((err) => console.log(err));
+  .then(() => {})
+  .catch((err) => console.log(err));
